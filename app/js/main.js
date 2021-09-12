f476d30894864a9297419dc2a8003406f6720317
Vanya-K <vanya.kim.96@gmail.com>
2021-09-13 01:00:06 +0300

    added slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function() {
-  // $('body').hide();
+  
+  $('.slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
 });
